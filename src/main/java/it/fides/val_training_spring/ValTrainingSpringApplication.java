cae94c425223ee6fa5a789d724f07278dd7c0ca8
Angelo Moreno <emilioangelo665@gmail.com>
2024-02-02 09:58:25 +0100

    Progetto creato

---
+++ src/main/java/it/fides/val_training_spring/ValTrainingSpringApplication.java
@@ -0,0 +1,13 @@
+package it.fides.val_training_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValTrainingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValTrainingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/fides/val_training_spring/ValTrainingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fides.val_training_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValTrainingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
