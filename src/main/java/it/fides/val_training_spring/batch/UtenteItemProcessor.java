c6d0887365c404070e057be73d4193d92f41c8d5
luca2002 <lucacipolla2002@gmail.com>
2024-02-15 14:01:15 +0100

    Revert "add Scheduler"
    This reverts commit 03d018a7fa2e511bdd7f03ee665c0d787e9e5e23.

--- src/main/java/it/fides/val_training_spring/batch/UtenteItemProcessor.java
+++ src/main/java/it/fides/val_training_spring/batch/UtenteItemProcessor.java
@@ -4,28 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import it.fides.val_training_spring.models.entities.UtenteEntity;
-import it.fides.val_training_spring.services.UtenteService;
-import it.fides.val_training_spring.services.GruppoService;
 
 public class UtenteItemProcessor implements ItemProcessor<UtenteEntity, UtenteEntity> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UtenteItemProcessor.class);
 
-    @Autowired
-	UtenteService utenteService;
-    
-    @Autowired
-	GruppoService gruppoService;
-    
     @Override
-    public UtenteEntity process(final UtenteEntity utente) throws Exception {
-    	
-    	UtenteEntity utenteEntity = utenteService.findByEmail(utente.getEmailUtente());
+    public UtenteEntity process(final UtenteEntity utente) {
+        String nomeUtente = utente.getNomeUtente().toUpperCase();
+        String cognomeUtente = utente.getCognomeUtente().toUpperCase();
+        String emailUtente = utente.getEmailUtente().toUpperCase();
 
-        return utenteEntity;
+        UtenteEntity transformedUtente = new UtenteEntity(nomeUtente, cognomeUtente, emailUtente);
+        LOGGER.info("Converting ( {} ) into ( {} )", utente, transformedUtente);
+
+        return transformedUtente;
     }
 
 }
--- src/main/java/it/fides/val_training_spring/models/entities/GruppoEntity.java
+++ src/main/java/it/fides/val_training_spring/models/entities/GruppoEntity.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,7 +37,7 @@ public class GruppoEntity {
 	@JoinColumn(name = "id_responsabile")
 	private UtenteEntity responsabile;
 	
-	@ManyToMany(fetch = FetchType.EAGER)
+	@ManyToMany
     @JoinTable(
             name = "utente_gruppo",
             joinColumns = @JoinColumn(name = "id_gruppo"),
